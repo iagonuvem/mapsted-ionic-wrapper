fb265ad498692e86be64bf3e2f43e9adf6dab276
Iago Nuvem <iagonuvem@hotmail.com>
2023-11-26 15:34:14 -0300

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mapsted-ionic-wrapper',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
