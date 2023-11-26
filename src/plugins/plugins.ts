b9c5e00f4ee33920f4c9c9eff517757c65efed7b
Iago Nuvem <iagonuvem@hotmail.com>
2023-11-26 19:18:11 -0300

    HOTFIX: Fix types

--- src/plugins/plugins.ts
+++ src/plugins/plugins.ts
@@ -2,7 +2,7 @@ import { registerPlugin } from '@capacitor/core';
 
 export interface MapstedIonicPluginI {
     echo(options: { value: string }): Promise<{ value: string }>;
-    getBuildingInfo(options: { value: number }): Promise<{ value: any }>;
+    getBuildingInfo(options: { buildingId: number }): Promise<{ BuildingInfo: any }>;
 }
 
 const MapstedIonicPlugin = registerPlugin<MapstedIonicPluginI>('MapstedIonicPlugin');
